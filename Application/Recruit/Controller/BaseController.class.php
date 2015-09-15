6926f8671bac7c84e4c35f4123bef4f572467639
momo <294666880@qq.com>
2015-09-15 13:57:53 +0800

    增加初始化方法，进行权限判断

--- Application/Recruit/Controller/BaseController.class.php
+++ Application/Recruit/Controller/BaseController.class.php
@@ -6,7 +6,40 @@ class BaseController extends CommonController {
  * 项目总控制器，直接继承CommonController
  */
 
-	const BASE 			= "BaseController";
+	const LOGIN_FROM 			= 'recruit/resume/login';
+
+	protected function _initialize(){
+
+		$CUR_ASK_FROM = strtolower(MODULE_NAME."/".CONTROLLER_NAME."/".ACTION_NAME);
+
+		if (strcmp(self::LOGIN_FROM, $CUR_ASK_FROM) == 0) {
+			
+			if (session('RESUME_LOGIN_FLAG')) {
+				// 重复登录，跳转到简历编辑页面
+				$this->error("您已经登录！", U('Recruit/Resume/edit'), 1);
+				
+				// 更新销毁session时间
+				session(array('name'=>'RESUME_INFO','expire'=>3600));
+				session(array('name'=>'RESUME_LOGIN_FLAG','expire'=>3600));
+			}else{
+				// 进入登录页面
+			}
+		}else{
+
+			if (session('RESUME_LOGIN_FLAG')) {
+				// 进入目标页面
+				
+				// 更新销毁session时间
+				session(array('name'=>'RESUME_INFO','expire'=>3600));
+				session(array('name'=>'RESUME_LOGIN_FLAG','expire'=>3600));
+			}else{
+
+				// 未登录，跳转到登录页面
+				$this->error("请先登录！", U(self::LOGIN_FROM), 1);
+			}
+		}
+	}
+
 }
 
 ?>
